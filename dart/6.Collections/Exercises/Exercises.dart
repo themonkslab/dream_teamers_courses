ad9a4f92b354fab908a177cf63a9b109a7c14a61
Nahuel Ferrero <nahuelferrero@gmail.com>
2023-03-20 07:58:29 -0300

    working on exercise 1 section 6

--- dart/6.Collections/Exercises/Exercises.dart
+++ dart/6.Collections/Exercises/Exercises.dart
@@ -1,26 +1,61 @@
+import 'dart:collection';
 import 'dart:io';
 
 void main () {
   // List of colors
-  List<String> colors = ["red", "blue", "green", "yellow", "black", "white"];
+  List <String> listOfColors =  ["red", "blue", "green", "yellow", "black", "white"];
+  List
 
-  // Map to store clothing items by color
-  Map<String, List<String>> clothingByColor = {};
+/*   // Map to store clothing items by color
+  //Map clothingByColor = <String, List<String>> {};
+  var clothingByColor = Map.fromIterable(listOfColors);
+  print(clothingByColor); */
 
   // Loop to prompt user to enter clothing items and colors
   while (true) {
-    stdout.write("Insert the clothing item (type 'done' to exit or 'clothes' to print the list): ");
-    String item = stdin.readLineSync().toString();
+    print("Insert the clothing item (type 'done' to exit or 'clothes' to print the list): ");
+    final item = stdin.readLineSync().toString();
 
     if (item.toLowerCase() == 'done') {
-      print('thanks for using Smart Closet');
+      print('\nthanks for using Smart Closet');
       break;
     }
 
-    stdout.write('Great! now choose a color: ');
+    print('Great! now choose a color from the list $listOfColors: ');
+    final colorInput = stdin.readLineSync().toString().toLowerCase();
 
-  }
+    if (listOfColors.contains(colorInput) == false) {
+      print("The color is not on the list. Do you want to add it (press 'y' to add or any to skip): ");
+      var addColor = stdin.readLineSync().toString().toLowerCase();
+      if (addColor == 'y') {
+        listOfColors.add(colorInput);
+      }
+    }
+
+    
 
 
 
+    /* for (var color in listOfColors) {
+      clothingByColor ['$color']= ''; 
+    } */
+    //print(listOfColors);
+
+
+
+    
+    }
+
+
+
+   // if (color == listOfColors[0]) {
+    //}
+
+    
 }
+
+
+
+
+
+
