d66dff6dbaef0375fa29e80f55bddcadca7af6d0
Nahuel Ferrero <nahuelferrero@gmail.com>
2023-04-03 15:55:46 -0300

    Translate article 12 section 9

--- dart/9.Functions/test.dart
+++ dart/9.Functions/test.dart
@@ -7,8 +7,19 @@
   print( peopleWithTitle);
 } */
 
-void main() {
+/* void main() {
   const people = ['Jack', 'Justin', 'Martin', 'Eugenio'];
   final  peopleWithTitle = people.map((person)=> 'Dr. $person');
   print( peopleWithTitle);
+} */
+
+void main() {
+  const list = [1, 2, 3];
+  print(transform(list, (x) => x * 2));
+  print(transform(list, (x) => x * 10));
+  print(transform(list, (x) => x / 2)); // 👁️ a problem arises here
+}
+
+List<int> transform(List<int> list, int Function(int) operation) {
+  return list.map(operation).toList();
 }
