be2ac0e97d1d13b6e84b744818fcbf6cf9489fcd
Mauro Di Bert <maurodibert@gmail.com>
2023-04-05 08:57:48 -0300

    fix futures, add async await with exercise

--- dart_sp/13.Async/general.dart
+++ dart_sp/13.Async/general.dart
@@ -1,37 +1,62 @@
+import 'package:test/test.dart';
+
 // void main() {
-//   group('Given a group of diners', () {
-//     test('When there are more than 4 diners, Then askForTable returns false',
-//         () {
-//       expectLater(askForTable(5), completion(isFalse));
+//   group('Given a printSeconds with N as value', () {
+//     test('When value is greater than 60, Then throws AssertionError', () {
+//       expectLater(printSeconds(value: 61, returnError: false),
+//           throwsA(isA<AssertionError>()));
 //     });
 
-//     test(
-//         'When there are less than or equal to 4 diners, Then askForTable returns true',
+//     test('When ends correctly, Then returns true after N seconds', () {
+//       expectLater(
+//           printSeconds(value: 2, returnError: false), completion(isTrue));
+//     });
+
+//     test('When something went wrong, Then throws an exception after N seconds',
 //         () {
-//       expectLater(askForTable(4), completion(isTrue));
+//       expectLater(printSeconds(value: 2, returnError: true), throwsException);
 //     });
 //   });
 // }
 
-Future<bool> askForTable(int diners) {
-  return Future.delayed(Duration(seconds: 3), () {
-    if (diners > 4) {
-      return false;
-    } else {
-      return true;
-    }
+void main() {
+  group('Given printSeconds function', () {
+    test('When value is greater than 60, Then throws AssertionError', () {
+      expect(() => printSeconds(value: 61, returnError: false),
+          throwsA(isA<AssertionError>()));
+    });
+
+    test('When returnError is true, Then throws Exception', () {
+      expect(() => printSeconds(value: 30, returnError: true),
+          throwsA(isA<Exception>()));
+    });
+
+    test('When all parameters are valid, Then returns true', () async {
+      final result = await printSeconds(value: 5, returnError: false);
+      expect(result, isTrue);
+    });
   });
 }
 
-void main() {
-  askForTable(5).then((value) {
-    if (value == true) {
-      print('You have a table! 🥳');
-    } else {
-      print('You don\'t have a table! 😋');
-    }
-    // El método catchError se ejecuta cuando se produce un error
-  }).catchError((error) {
-    print('Error: $error');
-  }).whenComplete(() => print('The process has finished! 🤓'));
+Future<bool> printSeconds({
+  required int value,
+  required bool returnError,
+}) async {
+  assert(value <= 60, 'Value must be less than 60 seconds');
+  if (returnError) throw Exception('Something went wrong!');
+  for (var i = 0; i < value; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    print('⏲ Seconds: ${i + 1}');
+  }
+  print('Done ⚡️');
+  return true;
 }
+
+// void main() async {
+//   try {
+//     await printSeconds(value: 10, returnError: false);
+//     await printSeconds(value: 10, returnError: true);
+//   } catch (e) {
+//     print(e);
+//   }
+// }
