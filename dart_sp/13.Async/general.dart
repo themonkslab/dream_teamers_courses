8552b41101c6f83ed9635066cacf3272b7930af4
Mauro Di Bert <maurodibert@gmail.com>
2023-04-11 08:38:12 -0300

    streams con capas

--- dart_sp/13.Async/general.dart
+++ dart_sp/13.Async/general.dart
@@ -1,31 +1,98 @@
 import 'dart:io';
 
-// void main() async {
-
-//   printResult(table);
-// }
-
-void printResult(bool value) {
+void printResult(bool value) async {
   if (value == true) {
-    print('You have a table! 🥳');
+    print('\nYou have a table! 🥳');
   } else {
-    print('You don\'t have a table! 😋');
+    print('\nYou don\'t have a table! 😋');
   }
-  print('We hope to see you soon! 🤗');
+  // Hago una pausa de 1 segundo para mostrar la despedida
+  await Future.delayed(
+      Duration(seconds: 1), () => print('Thanks for coming! 🤗'));
 }
 
 void main() async {
+  // Instancio el objeto de la capa de negocio
+  // y a su vez le paso el objeto de la capa de repositorio y a este,
+  // el objeto de la capa de datos
+  BusinessLogicObjectLayer businessLogicObjectLayer = BusinessLogicObjectLayer(
+    repository: RepositoryLayer(
+      data: DataLayer(),
+    ),
+  );
+
+  // Empezamos con el 'diseño de la vista'
+  // Recibo al usuario y le pregunto cuántas personas son
   stdout.write('Welcome to our restaurant! 🍽️\nHow many people are you? ');
   int diners = int.parse(stdin.readLineSync()!);
 
-  try {
-    bool table = await doWeHaveTable(diners);
-    printResult(table);
-  } catch (e) {
-    print('Error: ${e}');
+  // Esta variable la uso para mostrar el estado de carga
+  // y lo hago fuera ya que si lo hiciera dentro del listen
+  // se reiniciaría cada vez que se emita un nuevo evento
+  // y el while se ejecutaría infinitamente
+  var isLoading = false;
+
+  // Llamo al método de la capa de negocio
+  // y reacciono según los estados que esta me devuelva
+  businessLogicObjectLayer.doWeHaveTable(diners).listen((event) async {
+    if (event == BLoCStates.loading) {
+      stdout.write('Checking if we have a table for you');
+      isLoading = true;
+    } else if (event == BLoCStates.success) {
+      stdout.write('\nThe operation has been processed successfully');
+      isLoading = false;
+    } else {
+      isLoading = false;
+      printResult(await event as bool);
+    }
+    while (isLoading == true) {
+      stdout.write('.');
+      await Future.delayed(Duration(seconds: 1));
+    }
+  });
+}
+
+// BLoC significa Business Logic Component y en este enum
+// definimos los estados que puede tener la capa de negocio
+enum BLoCStates {
+  loading,
+  success,
+  error,
+}
+
+class BusinessLogicObjectLayer {
+  // Recibo el objeto de la capa de repositorio
+  // en el constructor de la capa de negocio
+  BusinessLogicObjectLayer({required this.repository});
+  final RepositoryLayer repository;
+
+  // Retorno un Stream<Object> porque en este caso
+  // necesito devolver el estado y el resultado de la operación
+  Stream<Object> doWeHaveTable(int diners) async* {
+    yield BLoCStates.loading;
+    Object table = await repository.doWeHaveTable(diners);
+    yield BLoCStates.success;
+    yield table;
   }
 }
 
-Future<bool> doWeHaveTable(int diners) {
-  return Future.error(Exception('Something went wrong! 😱'));
+class RepositoryLayer {
+  RepositoryLayer({required this.data});
+  final DataLayer data;
+
+  Future<bool> doWeHaveTable(int diners) async {
+    return await data.doWeHaveTable(diners);
+  }
+}
+
+class DataLayer {
+  Future<bool> doWeHaveTable(int diners) async {
+    // Va a simular que la base de datos tarda 5 segundos en responder
+    await Future.delayed(Duration(seconds: 5));
+    if (diners <= 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
